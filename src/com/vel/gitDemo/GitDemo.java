63921375fd5bad5f11084bdc1b3f9fc09d70dce7
pallavibhosale <bhosalepallavi2103@gmail.com>
2023-07-12 13:51:25 +0530

    First commit

---
+++ src/com/vel/gitDemo/GitDemo.java
@@ -0,0 +1,10 @@
+package com.vel.gitDemo;
+
+public class GitDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My first Git-Demo Project");
+	}
+
+}
